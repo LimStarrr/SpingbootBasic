197b5a4204fe8eff38aeeb1ec8bc843b673bd789
jongwani <jongwan.lim@iriver.com>
2018-04-27 14:42:33 +0900

    Web 추가

---
+++ src/main/java/com/basic/springboot/web/WebViewController.java
@@ -0,0 +1,20 @@
+package com.basic.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/")
+public class WebViewController {
+
+    @GetMapping
+    public String helloworld(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+
+}
